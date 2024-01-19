6731ae4732ccefe6bb6aa5c398de6743707241a9
Alexey <skulkyr20@gmail.com>
2024-01-20 00:05:51 +0300

    add field's

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,7 +7,7 @@ public class Application {
     public static void main(String[] args) {
 
         FilmStoreService service = FilmStoreService.INCTANCE;
-
+        //
         //Customer customer = service.createCustomer();
         //service.customerRentInventory(customer);
         //service.returnFilmToStore();
